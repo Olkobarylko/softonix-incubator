bd11a879c550950f61847b8486b7f0d0814a2b39
OlegBarylko <olegbarylko81@gmail.com>
2023-05-02 00:16:29 +0300

    some fixes

--- src/views/job-openings/JobOpening.vue
+++ src/views/job-openings/JobOpening.vue
@@ -1,79 +1,83 @@
 <template>
-    <div class="max-w-[450px]">
-        <el-select class="mb-5" v-model="selectValue" multiple filterable allow-create default-first-option
-            :reserve-keyword="false" placeholder="Choose tags for your article">
-            <el-option v-for="(item, index) in jobList()" :key="index" :label="item.department" :value="item.department" />
-        </el-select>
-        <p class="mb-2">Showing <span class="font-bold">{{ sum }}</span> out of  <span class="font-bold">{{ jobOpeningsOther().length }}</span> job openings</p>
-        <div class="w-[100%] h-[1px] bg-[#c9c9c9] mb-5"></div>
-        <ul class="custom-list" v-for="(item, index) in filterJobs" :key="index">
-            <h2 class="font-bold mb-4 pl-4">{{ item.department }} ({{ item.filterArray.length }})</h2>
-            <li class="text-light-blue pl-10 mb-2" v-for="(job, index) in item.filterArray" :key="index">
-                {{ Object.values(job)[0] }}
-            </li>
-            <button class="pl-4 pb-2 text-light-blue font-semibold">See More</button>
-        </ul>
-    </div>
+  <div class="max-w-[450px]">
+    <el-select
+      v-model="selectValue" class="mb-5" multiple filterable allow-create default-first-option
+      :reserve-keyword="false" placeholder="Choose tags for your article"
+    >
+      <el-option v-for="(item, index) in jobList()" :key="index" :label="item.department" :value="item.department" />
+    </el-select>
+    <p class="mb-2">
+      Showing <span class="font-bold">{{ sum }}</span> out of <span class="font-bold">{{
+        jobOpeningsOther().length }}</span> job openings
+    </p>
+    <div class="w-[100%] h-[1px] bg-[#c9c9c9] mb-5" />
+    <ul v-for="(item, index) in filterJobs" :key="index" class="custom-list">
+      <h2 class="font-bold mb-4 pl-4">{{ item.department }} ({{ item.filterArray.length }})</h2>
+      <li v-for="(job, index) in item.filterArray" :key="index" class="text-light-blue pl-10 mb-2">
+        {{ Object.values(job)[0] }}
+      </li>
+      <button class="pl-4 pb-2 text-light-blue font-semibold">See More</button>
+    </ul>
+  </div>
 </template>
-  
 <script lang="ts" setup>
 const jobOpeningsStore = useJobOpeningsStore()
 const { departments, jobOpenings } = storeToRefs(jobOpeningsStore)
 
-const selectValue = ref("")
+const selectValue = ref([])
 const sum = ref(jobOpenings.value.length)
 
-function jobOpeningsOther() {
-    return jobOpenings.value.map(item => {
-        if (item.departments.length === 0) {
-            item.departments = ['other'];
-        }
-        return item
-    })
+function jobOpeningsOther () {
+  return jobOpenings.value.map(item => {
+    if (item.departments.length === 0) {
+      item.departments = ['other']
+    }
+    return item
+  })
 }
 
-function jobList() {
-    const array: { department: string; filterArray: any[] }[] = []
+function jobList () {
+  const array: { department: string; filterArray: any[] }[] = []
 
-    departments.value.forEach((department) => {
-        const elevantJobList = jobOpeningsOther().filter((job) => {
-            return job.departments.includes(department.value)
-        })
-
-        if (elevantJobList.length > 0) {
-            const formattedDepartment = department.value
-                .replace(/_/g, ' ')
-                .replace(/\b\w/g, (c) => c.toUpperCase())
-
-            const sortedElevantJobList = elevantJobList.sort((a, b) =>
-                a.title.localeCompare(b.title)
-            )
-
-            const obj = { department: formattedDepartment, filterArray: sortedElevantJobList }
-            array.push(obj)
-        }
+  departments.value.forEach((department) => {
+    const elevantJobList = jobOpeningsOther().filter((job) => {
+      return job.departments.includes(department.value)
     })
-    return array
+
+    if (elevantJobList.length > 0) {
+      const formattedDepartment = department.value
+        .replace(/_/g, ' ')
+        .replace(/\b\w/g, (c) => c.toUpperCase())
+
+      const sortedElevantJobList = elevantJobList.sort((a, b) =>
+        a.title.localeCompare(b.title)
+      )
+
+      const obj = { department: formattedDepartment, filterArray: sortedElevantJobList }
+      array.push(obj)
+    }
+  })
+  return array
 }
 
 const filterJobs = computed(() => {
-    let couner = 0
-    if (selectValue.value.length === 0) {
-        sum.value = jobOpenings.value.length
-        return jobList()
-    }
+  let couner = 0
+  if (selectValue.value.length === 0) {
+    sum.value = jobOpenings.value.length
+    return jobList()
+  }
 
-    const result = jobList().filter(item => {
-        return selectValue.value.includes(item.department)
+  const result = jobList().filter(item => {
+    return selectValue.value.includes(item.department)
+  })
+
+  if (selectValue.value.length > 0) {
+    result.forEach(item => {
+      couner = couner + item.filterArray.length
+      sum.value = couner
     })
-
-    if (selectValue.value.length > 0) {
-        result.forEach(item => {
-            couner = couner + item.filterArray.length
-            sum.value = couner
-        })
-    }
-    return result
+  }
+  return result
 })
 
 </script>
